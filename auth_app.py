a9f0513da7e3f9681c26a3ed7624a61d23594ae3
anmolUG <155796129+anmolUG@users.noreply.github.com>
2025-04-24 12:42:07 +0530

    Update auth_app.py

--- auth_app.py
+++ auth_app.py
@@ -8,6 +8,7 @@ from datetime import datetime
 # Configure page
 st.set_page_config(page_title="RideWise", layout="centered")
 print("hello")
+print("yo")
 # Custom CSS Styling
 # Custom CSS for style
 st.markdown("""
